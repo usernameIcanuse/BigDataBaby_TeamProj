18e670b2bea88a3817d422a0c477bb27fc717fdb
usernameIcanuse <90302406+usernameIcanuse@users.noreply.github.com>
2023-11-18 12:45:36 +0900

    search,detail css 추가, 적용

--- php/Detail.php
+++ php/Detail.php
@@ -12,7 +12,7 @@ else
 {
 
 
-	echo "Restaurant: ".$_SESSION['NAME']."<br/>";
+	echo "<h1 style=\"text-align: Left;\">".$_SESSION['NAME']."</h1><br/>";
 	echo "Phone Number: ";
 
 	$sql = "select * from Contact
@@ -30,14 +30,14 @@ else
 		{
 			while($newArray = mysqli_fetch_array($res))
 			{
-				echo $newArray['PhoneNum']."<br/>";
+				echo "<strong>".$newArray['PhoneNum']."</strong><br/>";
 			}
 		}
 		mysqli_free_result($res);
 	}
 
-	$sql = "select * from hashtag A inner join restaurant B inner join on A.hashtag_code = B.hashtag_code inner join Available C on B.Code = C.Code inner join BreakTime D on B.Code = D.Code inner join rating E on B.Code = E.Code 
-		where Code = ?";
+	$sql = "select * from hashtag A inner join restaurant B on A.hashtag_code = B.hashtag_code inner join Available C on B.Code = C.Code inner join BreakTime D on B.Code = D.Code inner join rating E on B.Code = E.Code 
+		where B.Code = ?";
 
 	if($stmt = mysqli_prepare($mysqli, $sql))
 	{
@@ -51,16 +51,16 @@ else
 		{
 			while($newArray = mysqli_fetch_array($res))
 			{
-				echo "Available :".$newArray['Start']." ~ ".$newArray['End']."<br/>";
-				echo "BreakTime :".$newArray['BreakStart']." ~ ".$newArray['BreakEnd']."<br/>";
-				echo "Rating : ".$newArray['rate']." rate : ".$newArray['rate']."<br/>";
+				echo "Available :".$newArray['Start']." AM ~ ".$newArray['End']." PM<br/>";
+				echo "BreakTime :".$newArray['BreakStart']." PM ~ ".$newArray['BreakEnd']."<br/>";
+				echo "Rate : ⭐".$newArray['RATE']."<br/>";
 			}
 		}
 		mysqli_free_result($res);
 	}
 
 
-	echo "</br>Menu<br/>";
+	echo "</br><h2 style =\"text-align: center;\">Menu</h1><br/>";
 	$sql = "select * from menu
 		where Code = ?";
 
@@ -74,6 +74,7 @@ else
 
 		if($res = mysqli_stmt_get_result($stmt))
 		{
+			
 			while($newArray = mysqli_fetch_array($res))
 			{
 				echo $newArray['Menu_Name']."     ".$newArray['Menu_Prices']." Won"."<br/>";
--- php/SearchRestaurant.php
+++ php/SearchRestaurant.php
@@ -1,44 +1,42 @@
 <?php
 session_start();
 
-$mysqli = mysqli_connect("localhost","team05","team05","team05");
+$mysqli = mysqli_connect("localhost", "team05", "team05", "team05");
 
-if(mysqli_connect_errno())
-{
-	printf("Connect failed: %s\n",mysqli_connect_error());
-	exit();
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+} else {
+    $sql = "SELECT Name, Type, A.Code as Code, B.Code as TypeCode FROM Restaurant A INNER JOIN Types B ON A.TypeCode = B.Code WHERE Name = ? AND Type = ?";
+
+    if ($stmt = mysqli_prepare($mysqli, $sql)) {
+        mysqli_stmt_bind_param($stmt, "ss", $first, $second);
+
+        $first = $_SESSION['NAME'];
+        $second = $_SESSION['TYPE'];
+
+        mysqli_stmt_execute($stmt);
+
+        if ($res = mysqli_stmt_get_result($stmt)) {
+            echo '<table class="result-table">';
+            echo '<thead><tr><th>Name</th><th>Type</th></tr></thead>';
+            echo '<tbody>';
+
+            while ($newArray = mysqli_fetch_array($res)) {
+                echo '<tr>';
+                echo '<td>' . $newArray['Name'] . '</td>';
+                echo '<td>' . $newArray['Type'] . '</td>';
+                echo '</tr>';
+                $_SESSION['CODE'] = $newArray['Code'];
+            }
+
+            echo '</tbody>';
+            echo '</table>';
+        }
+
+        mysqli_free_result($res);
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($mysqli);
 }
-else
-{
-
-
-	$sql = "select Name, Type, A.Code as Code, B.Code as TypeCode  from Restaurant A inner join Types B on A.TypeCode = B.Code
-		where Name = ? AND Type = ?";
-
-	if($stmt = mysqli_prepare($mysqli, $sql))
-	{
-		mysqli_stmt_bind_param($stmt, "ss", $first, $second);
-
-		$first = $_SESSION['NAME'];
-		$second = $_SESSION['TYPE'];
-		
-		mysqli_stmt_execute($stmt);
-
-		if($res = mysqli_stmt_get_result($stmt))
-		{
-			while($newArray = mysqli_fetch_array($res))
-			{
-				echo "Name: ".$newArray['Name']." Type: ".$newArray['Type']."\n";
-				$_SESSION['CODE'] = $newArray['Code'];
-
-			}
-		}
-		mysqli_free_result($res);
-	}
-	mysqli_stmt_close($stmt);
-	mysqli_close($mysqli);
-
-}
-
-
 ?>
