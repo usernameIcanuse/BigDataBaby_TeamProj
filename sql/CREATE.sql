ac9a79d8033b2d410e404e51ca54d848e5adf1c8
usernameIcanuse <90302406+usernameIcanuse@users.noreply.github.com>
2023-11-18 13:23:45 +0900

    sql 수정. create rating,delivery 추가

--- sql/CREATE.sql
+++ sql/CREATE.sql
@@ -62,3 +62,81 @@ CREATE TABLE Review (Code VARCHAR(25),
 						FOREIGN KEY (Code) REFERENCES restaurant(Code));
                         
 CREATE INDEX idx_Code ON Review (review_code);
+
+CREATE TABLE delivery AS
+SELECT * FROM restaurant WHERE 1=0;
+
+INSERT INTO delivery (CODE, NAME, TYPECODE)
+SELECT CODE, NAME, TYPECODE FROM restaurant;
+
+ALTER TABLE delivery
+DROP COLUMN NAME;
+
+ALTER TABLE delivery
+DROP COLUMN TYPECODE;
+
+ALTER TABLE delivery
+DROP COLUMN hashtag_code;
+
+ALTER TABLE DELIVERY
+ADD COLUMN dlvy_avail BOOLEAN;
+
+ALTER TABLE DELIVERY
+ADD COLUMN  dlvy_price int;
+
+ALTER TABLE DELIVERY
+ADD COLUMN dlvy_ok_price int;
+
+UPDATE DELIVERY
+SET DLVY_PRICE = FLOOR(RAND()*10)*1000;
+
+UPDATE DELIVERY
+SET DLVY_OK_PRICE = FLOOR(RAND()*10)*1000 + 5000;
+
+UPDATE DELIVERY
+SET DLVY_AVAIL = ROUND(RAND());
+
+UPDATE delivery
+SET dlvy_price = FLOOR(RAND() * (15 - 8 + 1) + 8) * 1000
+WHERE dlvy_price BETWEEN 8000 AND 15000;
+
+
+UPDATE DELIVERY
+SET DLVY_PRICE = NULL, DLVY_OK_PRICE = NULL
+WHERE DLVY_AVAIL = 0;
+
+-- restaurant 테이블 구조를 복사하여 rating 테이블 생성
+CREATE TABLE rating AS
+SELECT * FROM restaurant WHERE 1=0;
+
+-- rating 테이블에 restaurant 테이블의 데이터 복사
+INSERT INTO rating (CODE, NAME, TYPECODE)
+SELECT CODE, NAME, TYPECODE FROM restaurant;
+
+-- TYPECODE 컬럼 삭제
+ALTER TABLE RATING
+DROP COLUMN TYPECODE;
+
+ALTER TABLE RATING
+DROP COLUMN NAME;
+
+ALTER TABLE RATING
+DROP COLUMN hashtag_code;
+-- RATE 컬럼 추가
+ALTER TABLE RATING
+ADD COLUMN RATE FLOAT;
+
+-- RATE 컬럼의 값을 0부터 10까지 랜덤 소수점 값으로 업데이트
+UPDATE RATING
+SET RATE = ROUND(RAND() * 10, 1);
+
+ALTER TABLE rating
+ADD COLUMN total_rating INT(4);
+
+UPDATE rating
+SET total_rating = FLOOR(100 + RAND() * (9999 - 100 + 1));
+
+
+
+
+
